1af5318f73fc6d3fcd37f903dc8aa4939d99ec78
Shjsx2 <s4jin@ucsd.edu>
2021-02-25 10:47:24 -0800

    thirdparty API lab (map, camera, rsvp)

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'TnvU7anU2aOuT1jRoNrZpRW5OlsIT18D'
+	var map = L.mapquest.map('map' , {
+		center: [32.882 , -117.234], 
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+	L.marker([32.882 , -117.234]).addTo(map);
 }
